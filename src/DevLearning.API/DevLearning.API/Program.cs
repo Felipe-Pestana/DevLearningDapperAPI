31a504e45e4292357d96fc8ac874537db1868703
gabriela-fernanda <gabrielafernanda0113@gmail.com>
2025-11-29 12:45:43 -0300

    Criação da classe Category

--- src/DevLearning.API/DevLearning.API/Program.cs
+++ src/DevLearning.API/DevLearning.API/Program.cs
@@ -1,8 +1,18 @@
+using DevLearning.API.Repositories;
+using DevLearning.API.Repositories.Interfaces;
+using DevLearning.API.Services;
+using DevLearning.API.Services.Interfaces;
+using System.Data.Common;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddScoped<DevLearning.API.DataBase.DbConnection>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
 
 var app = builder.Build();
 
