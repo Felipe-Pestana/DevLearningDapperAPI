913a0cbf9f07b89573413e7c363b7591fa38fc24
Brunovini08 <brunovinicius082005@gmail.com>
2025-11-29 12:38:41 -0300

    refact: Alterando o nome da classe de conexao com o banco

--- src/DevLearning.API/DevLearning.API/Repositories/StudentRepository.cs
+++ src/DevLearning.API/DevLearning.API/Repositories/StudentRepository.cs
@@ -1,6 +1,21 @@
-﻿namespace DevLearning.API.Repositories
+﻿using DevLearning.API.DataBase;
+using DevLearning.API.Models;
+using DevLearning.API.Repositories.Interfaces;
+using Microsoft.Data.SqlClient;
+using System.Data.Common;
+
+namespace DevLearning.API.Repositories
 {
-    public class StudentRepository
+    public class StudentRepository : IStudentRepository
     {
+        private readonly SqlConnection _connection;
+        public StudentRepository(ConnectionDB dbConnection)
+        {
+            _connection = dbConnection.GetConnection();
+        }
+        public Task CreateStudent(Student student)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
