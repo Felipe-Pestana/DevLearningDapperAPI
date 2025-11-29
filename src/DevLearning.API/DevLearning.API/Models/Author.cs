3841d231fc539ede87047566a3037b7041c8b0d2
anacarolina-py <anacarolinaalvez_@hotmail.com>
2025-11-29 01:13:16 -0300

    Adicao classe Author, DTOS, Repository e Service. Att program com builders ref a author

--- src/DevLearning.API/DevLearning.API/Models/Author.cs
+++ src/DevLearning.API/DevLearning.API/Models/Author.cs
@@ -2,5 +2,24 @@
 {
     public class Author
     {
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public string Title { get; private set; }
+        public string Image {  get; private set; }
+        public string Bio { get; private set; }
+        public string Url { get; private set; }
+        public string Email { get; private set; }
+        public bool Type { get; private set; } //Author ativo ou inativo
+
+        public Author(string name, string title, string image, string bio, string url, string email, bool type)
+        {
+            Name = name;
+            Title = title;
+            Image = image;
+            Bio = bio;
+            Url = url;
+            Email = email;
+            Type = true;
+        }
     }
 }
--- src/DevLearning.API/DevLearning.API/Program.cs
+++ src/DevLearning.API/DevLearning.API/Program.cs
@@ -1,10 +1,24 @@
+using DevLearning.API.DataBase;
+using DevLearning.API.Repositories;
+using DevLearning.API.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 
+builder.Services.AddSingleton<DbConnection>();
+
 var app = builder.Build();
+
+builder.Services.AddSingleton<AuthorRepository>();
+builder.Services.AddSingleton<AuthorService>();
+
+
+
+
+
 
 // Configure the HTTP request pipeline.
 
