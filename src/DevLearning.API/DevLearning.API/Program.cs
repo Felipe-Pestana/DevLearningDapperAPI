6dd6e78e3e713e9cbf9bc2c7dff0d53f7977a6dd
Wayne Junior <120405692+waynemcjr@users.noreply.github.com>
2025-11-30 00:03:12 -0300

    Update Program.cs

--- src/DevLearning.API/DevLearning.API/Program.cs
+++ src/DevLearning.API/DevLearning.API/Program.cs
@@ -11,7 +11,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddSingleton<ConnectionDb>();
 builder.Services.AddSingleton<CourseRepository>();
 builder.Services.AddSingleton<CourseService>();
 
